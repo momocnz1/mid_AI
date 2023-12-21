8c2fafebea3b60602061b46ad7bd98e04f94cb59
momocn <119989172+momocnz1@users.noreply.github.com>
2023-12-21 12:00:35 +0700

    1

--- mid.py
+++ mid.py
@@ -10,29 +10,29 @@ File_Name ='car_data.csv'
 df = pd.read_csv(File_Path + File_Name)
 
 
-df.dropna(columns=['User ID'],inplace=True)
+df.drop(columns=['User ID'],inplace=True)
 encoders = []
 for i in range(0,len(df.columns)):
     en = LabelEncoder()
     df.iloc[:,i] = en.fit_transform(df.iloc[:,i])
     encoders.append(en)
 
-x = df.iloc[:,0:4]
+x = df.iloc[:,0:3]
 y = df['Purchased']
 
 x_train,x_test,y_train,y_test = train_test_split(x,y, random_state=0)
 
 model = DecisionTreeClassifier(criterion='entropy')
-model.fit(x,y)
+model.fit(x_train,y_train)
 x_pred = ['Male','35','20000']
 
 for i in range(0,len(df.columns)):
          x_pred[i] = encoders[i].transform([x_pred[i]])
          x_pred_adj = np.arry(x_pred).reshape(-1,5)
-         
+        
 y_pred = model.predict(x_pred_adj)
 print('Prediiction:', y_pred[0])
-score = model.score(X, y)
+score = model.score(x_train,y_train)
 print('Accuracy:','{:.2f}'.format(score))
 
 print(model.score(x_train, y_train))
